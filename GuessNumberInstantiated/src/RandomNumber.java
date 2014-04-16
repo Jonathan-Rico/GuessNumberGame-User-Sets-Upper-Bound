1fd80381f021ee76fab494752f57d9bc56cd6cf8
Jonathan-Rico <jonyrico231@gmail.com>
2014-04-15 21:32:33 -0700

    Update RandomNumber.java

--- GuessNumberInstantiated/src/RandomNumber.java
+++ GuessNumberInstantiated/src/RandomNumber.java
@@ -3,4 +3,8 @@ public class RandomNumber {
 		int x =	(int) (1 + Math.random() * high);
 		return x;
 	}
+	public int GetANumber_Between_1_and_10 () {
+	int notUsed =	(int) (1 + Math.random() * 10);
+	return notUsed;
+	}
 }
